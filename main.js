fead8796a342da067ec35a3ef16e2039d8a98e78
Do Nam Binh <131113311+Do-Nam-Binh@users.noreply.github.com>
2023-07-06 16:09:38 +0700

    Add color change on mouse hover

--- main.js
+++ main.js
@@ -1,29 +1,24 @@
 const container = document.querySelector('.container');
-const row = document.createElement('div');
-row.classList.add('row');
+const grid = document.createElement('div');
+grid.classList.add('grid');
 
 
 const onePixel = document.createElement('div');
 onePixel.classList.add('onePixel');
-onePixel.addEventListener('mouseenter', this.activatePixel);
 
-function activatePixel(){
-    this.classList.remove('onePixel');
-    this.classList.add('activatedPixel');
-}
+grid.addEventListener('mouseover', e => e.target.classList.add('activatedPixel'));
+
 
 
 
 function makeGrid(pixel){
-    for(let j = 0; j < pixel; j++){            
-        
-        row.appendChild(onePixel.cloneNode(true));
-    }
     
-    for(let i = 0; i < pixel; i++){
+    for(let j = 0; j < pixel*pixel; j++){            
         
-        container.appendChild(row.cloneNode(true));
+        grid.appendChild(onePixel.cloneNode(true));
     }
+    container.appendChild(grid)
+    grid.style.gridTemplateColumns = "repeat(" + pixel + ", 1fr)";
 }
 
-makeGrid(16);
+makeGrid(5);

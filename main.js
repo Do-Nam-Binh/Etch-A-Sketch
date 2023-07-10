9396658ca72bde29db20359e7799d4b39cecc053
Do Nam Binh <131113311+Do-Nam-Binh@users.noreply.github.com>
2023-07-10 17:18:30 +0700

    Add default grid at startup

--- main.js
+++ main.js
@@ -35,5 +35,31 @@ function makeGrid(){
     grid.classList.remove('activatedPixel');
 }
 
+function defaultGrid(){
+    
+    pixel = 16
+
+    if(!Number.isInteger(parseInt(pixel))){
+        alert("Invalid value");
+        return;
+    }
+
+    if(pixel > 100 || pixel < 1){
+        alert("Please enter a value between 1 and 100");
+        return;
+    }
+
+    grid.replaceChildren();
+
+    for(let j = 0; j < pixel*pixel; j++){            
+        
+        grid.appendChild(onePixel.cloneNode(true));
+    }
+    container.appendChild(grid)
+    grid.style.gridTemplateColumns = "repeat(" + pixel + ", 1fr)";
+    grid.classList.remove('activatedPixel');
+}
+
+defaultGrid();
 const button = document.querySelector('.setSize');
 button.addEventListener('click', makeGrid);
